1c2e7d6b9d0366cd42fce468577909148d946e0e
Manjeet Kumar <37359630+manjeetkmr18@users.noreply.github.com>
2022-10-13 08:51:59 -0400

    added post api

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const config = require("./config");
+const userSchema = require("./models/userModel");
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.set("view engine", "ejs");
@@ -25,6 +30,20 @@ app.get("/g2test", (req, res) => {
 app.get("/gtest", (req, res) => {
   res.render("gtest");
 });
+
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+
+app.post("/g2test/saveuser", (req, res) => {
+  console.log(req.body);
+  userSchema.create(req.body, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+  res.redirect("/");
 });
